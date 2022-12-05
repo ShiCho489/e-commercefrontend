093f09eb101cd724cda21a8515c6c5f3cdf73737
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-05 23:17:10 +0200

    Updates

---
+++ precache-manifest.5e8c364f3365c5b72653743ecb268333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259397cc775316267d545f14eda6c94b",
+    "url": "/e-commercefrontend/index.html"
+  },
+  {
+    "revision": "d73491028409edb942e7",
+    "url": "/e-commercefrontend/static/css/main.069c3d0d.chunk.css"
+  },
+  {
+    "revision": "313ccda13af906be5d2b",
+    "url": "/e-commercefrontend/static/js/2.493e2401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-commercefrontend/static/js/2.493e2401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73491028409edb942e7",
+    "url": "/e-commercefrontend/static/js/main.1f017b0b.chunk.js"
+  },
+  {
+    "revision": "fb99e7f3f7631c9b7ebd",
+    "url": "/e-commercefrontend/static/js/runtime-main.05063eb7.js"
+  }
+]);
